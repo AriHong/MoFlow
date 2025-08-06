f26672d616e2df4fbfcbf87c4d13e74a8bd9eda1
AriHong <elly924@snu.ac.kr>
2025-08-06 17:17:11 +0900

    Add eval_dtw module and update __init__.py for import exposure

--- src/MoFlow/__init__.py
+++ src/MoFlow/__init__.py
@@ -4,6 +4,7 @@ from .plot_gene import dynamic_plot
 from .plot_cell import scatter_cell
 from .eval import cross_boundary_correctness
 from .eval import inner_cluster_coh
+from .eval_dtw import plot_dtw
 from .simulation import simulate
 
 __all__ = [
